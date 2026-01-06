efbbff842956e8c76ad3e8b4ed3eb744ff32bde2
Peter <161234440+Peter-diper@users.noreply.github.com>
2026-01-06 03:51:50 +0330

    finshing ther project

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,145 @@
-async function data() {
-  const url = "db.json";
-  const table = document.querySelector("#table");
+// inputs
+const searchInput = document.querySelector("#search");
+const btnStart = document.querySelector("#btn-calc");
+const searchSection = document.querySelector(".search-wrapper");
+const mainSection = document.querySelector(".content--section");
+const overlay = document.querySelector(".overlay");
 
-  const response = await fetch(url);
-  const result = (await response.json()).transactions;
+btnStart.addEventListener("click", startCalc);
 
-  console.log(result);
-  const id = finder(result, "id");
-  const type = finder(result, "type");
-  const price = finder(result, "price");
-  const refId = finder(result, "refId");
-  const date = finder(result, "date");
+// events
+document.addEventListener("DOMContentLoaded", () => {
+  data();
+});
+searchInput.addEventListener("input", search);
 
-  let tableInfo = `<tr class="">
-            <th>ردیف</th>
-            <th>نوع تراکنش</th>
-            <th>مبلغ</th>
-            <th>شماره پیگیری</th>
-            <th>تاریخ تراکنش</th>
-          </tr>`;
-  result.forEach((value, i) => {
-    tableInfo += `<tr>
-            <td>${id[i]}</td>
-            <td id="type">${type[i]}</td>
-            <td>${price[i]}</td>
-            <td>${refId[i]}</td>
-            <td>${date[i]}</td>
-          </tr>`;
-  });
-  table.innerHTML = tableInfo;
-
-  // changing type color by dinamic
-  const transactionsType = document.querySelectorAll("#type");
-  transactionsType.forEach((node) => {
-    node.textContent === "افزایش اعتبار"
-      ? node.classList.add("resive")
-      : node.classList.add("take");
-  });
+// funcotions
+function data() {
+  const url = "https://your-project.vercel.app/api/transactions";
+  render(url);
 }
 
 function finder(data, type) {
   return data.map((content) => content[type]);
 }
 
-data();
+function search(e) {
+  const url = `http://localhost:3000/transactions?refId_like=${e.target.value}`;
+  render(url);
+}
+
+async function render(url) {
+  const table = document.querySelector("#table");
+
+  const response = await fetch(url);
+  const result = await response.json();
+
+  const id = finder(result, "id");
+  const type = finder(result, "type");
+  const price = finder(result, "price");
+  const refId = finder(result, "refId");
+  const date = finder(result, "date");
+
+  const locaDate = date.map((value) =>
+    new Date(value).toLocaleDateString("fa-ir")
+  );
+
+  let tableInfo = "";
+  result.forEach((value, i) => {
+    tableInfo += `<tr>
+            <td>${id[i]}</td>
+            <td id="type">${type[i]}</td>
+            <td>${price[i]}</td>
+            <td id="redId">${refId[i]}</td>
+            <td>${locaDate[i]}</td>
+          </tr>`;
+  });
+  table.innerHTML = tableInfo;
+
+  // changing type color by dinamic
+  const transactionsType = document.querySelectorAll("#type");
+
+  transactionsType.forEach((node) => {
+    node.textContent === "افزایش اعتبار"
+      ? node.classList.add("resive")
+      : node.classList.add("take");
+  });
+
+  const orderPriceBtn = document.querySelector("#priceBtn");
+  orderPriceBtn.addEventListener("click", orderPrice);
+  const datePriceBtn = document.querySelector(".date");
+  datePriceBtn.addEventListener("click", orderDate);
+}
+
+function orderPrice() {
+  const orderPriceBtn = document.querySelector("#priceBtn");
+  const tDetail = document.querySelectorAll("td");
+  const arrow = document.querySelector("#price-arrow");
+
+  if (!arrow.classList.contains("arrow--active")) {
+    arrow.classList.add("arrow--active");
+    setTimeout(() => {
+      const url = "http://localhost:3000/transactions?_sort=price&_order";
+      render(url);
+      arrow.classList.add("arrow--active");
+    }, 200);
+    tDetail.forEach((node) => {
+      node.classList.add("opacity--animatin");
+    });
+
+    console.log(orderPriceBtn.ariaSelected);
+    return;
+  }
+  console.log("true");
+  setTimeout(() => {
+    const url = "http://localhost:3000/transactions?_sort=price&_order=desc";
+    render(url);
+  }, 200);
+  arrow.classList.remove("arrow--active");
+
+  tDetail.forEach((node) => {
+    node.classList.add("opacity--animatin");
+  });
+}
+
+function orderDate() {
+  const orderPriceBtn = document.querySelector("#priceBtn");
+  const tDetail = document.querySelectorAll("td");
+  const arrow = document.querySelector("#date-arrow");
+
+  if (!arrow.classList.contains("arrow--active")) {
+    arrow.classList.add("arrow--active");
+    setTimeout(() => {
+      const url = "http://localhost:3000/transactions?_sort=date&_order";
+      render(url);
+      arrow.classList.add("arrow--active");
+    }, 200);
+    tDetail.forEach((node) => {
+      node.classList.add("opacity--animatin");
+    });
+
+    console.log(orderPriceBtn.ariaSelected);
+    return;
+  }
+  console.log("true");
+  setTimeout(() => {
+    const url = "http://localhost:3000/transactions?_sort=date&_order=desc";
+    render(url);
+  }, 200);
+  arrow.classList.remove("arrow--active");
+
+  tDetail.forEach((node) => {
+    node.classList.add("opacity--animatin");
+  });
+}
+
+function startCalc(e) {
+  e.target.classList.add("hidden");
+  overlay.classList.remove("hidden");
+
+  setTimeout(() => {
+    searchSection.classList.remove("hidden");
+    mainSection.classList.remove("hidden");
+    overlay.classList.add("hidden");
+  }, 2000);
+}
