986b4fc0ff8e46efa704b0c1517a28f30fd3347d
Peter <161234440+Peter-diper@users.noreply.github.com>
2026-01-06 04:39:33 +0330

    test vercel

--- api/transactions.js
+++ api/transactions.js
@@ -1,5 +1,9 @@
+// // api/transactions.js
 import data from "../db.json";
 
+// export default function handler(req, res) {
+//   res.status(200).json(data.transactions);
+// }
 export default function handler(req, res) {
-  res.status(200).json(data.transactions);
+  res.status(200).json({ ok: true });
 }
